aeab6ca9a3eb49a108253efabb668efc565441d5
User <totowka82@mail.ru>
2019-04-30 12:39:46 +0400

    add summary <p>

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,7 @@ const searchInput = document.querySelector('input')
 const mes1 = document.querySelector('#mes1')
 const mes2 = document.querySelector('#mes2')
 const mes3 = document.querySelector('#mes3')
+const mes4 = document.querySelector('#mes4')
 
 weatherForm.addEventListener('submit', (event)=>{
     event.preventDefault()
@@ -13,6 +14,7 @@ weatherForm.addEventListener('submit', (event)=>{
     mes1.textContent = 'loading...'
     mes2.textContent = ''
     mes3.textContent = '' 
+    mes4.textContent = '' 
 
     fetch('/weather?search=' + location).then((response)=>{
     response.json().then((data)=>{
@@ -22,8 +24,9 @@ weatherForm.addEventListener('submit', (event)=>{
             mes2.textContent = data.err_message 
         }else{
             mes1.textContent = data.location.loc_name 
-            mes2.textContent = 'temperature: ' + data.temperature
-            mes3.textContent = 'wind speed: ' + data.windSpeed 
+            mes2.textContent = 'Today is  ' + data.summary 
+            mes3.textContent = 'temperature: ' + data.temperature
+            mes4.textContent = 'wind speed: ' + data.windSpeed 
         }
     })
 })
